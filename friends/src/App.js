b509a3319289c52476e28af760cd23e8b7ab5645
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-03-17 20:24:22 -0400

    setting up needed files

---
+++ friends/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Login from "./components/Login";
+import GasPrices from "./components/GasPrices";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={GasPrices} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
+// Please don't mind me
